324ad5263558a1cecc2365147e1a6444300c7153
Robbie Hughes <rwhughes91@gmail.com>
2020-10-29 23:56:15 -0700

    global spelling check

--- src/components/scenes/Projects/ProjectsScene.tsx
+++ src/components/scenes/Projects/ProjectsScene.tsx
@@ -14,37 +14,37 @@ const ProjectsScene: React.FC = () => {
         <div className={classes.content}>
           <Row customClassNames={classes.cards}>
             <Card
-              image="ttlbl"
-              name="New Jersey Tax Lien Database"
-              shortDescription="A web app/accounting tool for acquired tax liens in the state of New Jersey."
-              description="A web app/accounting tool for acquired tax liens in the state of New Jersey. We used this tool for both accounting and financial analysis."
-              tech="React, Apollo Server (graphql), Mongodb, Redux, Redux-Saga, Socket.io, Sendgrid/Twilio, AWS S3, CSS Modules, PostCss, Webpack, Babel, Jest, Heroku."
-              auth={[
-                'Authentication is required to use app.',
-                ' Please contact me if you wish to see a demo.',
-              ]}
-              url="https://new-jersey-database.herokuapp.com/"
-            />
-            <Card
               image="sdr"
               name="Super Delicious Records"
               shortDescription="A record label e-commerce web application."
               description="A record label e-commerce web application that primarily manages metal, punk, and hard rock bands. This app also includes an admin interface (inspired by django-admin) to easily manage and add data."
-              tech="React, Next.js, Apollo Micro Server (graphql), Firebase, Stripe, Sendgrid/Twilio, AWS S3, AWS Cloudfront, Scss, Css Modules, PostCss, Typescript, Webpack, Babel, Jest, Vercel"
+              tech="JavaScript/TypeScript, React, Next.js, Apollo Micro Server (GraphQL), Firebase, Stripe, SendGrid/Twilio, AWS S3, AWS CloudFront, SCSS, CSS Modules, PostCSS, Webpack, Babel, Jest, Vercel"
               url="https://super-delicious-records-nvn09ff67.vercel.app/"
             />
             <Card
               image="colorng"
               name="ColorNg"
-              description="A react native application that lets you create and share color gradients. You can create gradients manually, or use images to create gradients. Every morning a deep learning model creates 4 gradients that are featured for the day."
+              description="A react native application that lets you create and share color gradients. You can create gradients manually, or use images to create gradients. Every morning 4 gradients are created and featured for the day."
               shortDescription="A react native application that lets you create and share color gradients."
-              tech="React, React Native, Firebase, Sqlite, Redux, Redux Thunk, AWS Lambda, Typescript, Metro, Babel, Jest, Expo"
+              tech="JavaScript/TypeScript, React, React Native, Firebase, SQLite, Redux, Redux Thunk, AWS Lambda, Metro, Babel, Jest, Expo"
               auth={[
-                'You need to use the expo client app to preview this application if you are using an iphone.',
+                'You need to use the expo client app to preview this application if you are using an iPhone.',
                 ' Please contact me if you have any questions!',
               ]}
               url="https://expo.io/@rwhughes91/projects/colorNg"
             />
+              <Card
+                image="ttlbl"
+                name="New Jersey Tax Lien Database"
+                shortDescription="A web app/accounting tool for acquired tax liens in the state of New Jersey."
+                description="A web app/accounting tool for acquired tax liens in the state of New Jersey. We used this tool for both accounting and financial analysis."
+                tech="JavaScript, React, Apollo Server (GraphQL), MongoDB, Redux, Redux-Saga, Socket.io, SendGrid/Twilio, AWS S3, CSS Modules, PostCSS, Webpack, Babel, Jest, Heroku."
+                auth={[
+                  'Authentication is required to use app.',
+                  ' Please contact me if you wish to see a demo.',
+                ]}
+                url="https://new-jersey-database.herokuapp.com/"
+              />
             <Card
               image="odt"
               name="Odonate Therapeutics"
@@ -62,9 +62,9 @@ const ProjectsScene: React.FC = () => {
             <Card
               image="drugCurves"
               name="Drug Curves"
-              description="A python project that organizes, analyzes, and generates pdf reports for several U.S. drug markets, including alk inhibitors, metastatic breast cancer drugs, norepinephrine and vasopressin, oral iron chelators, nk1 receptor antagonists and 5-ht3 receptor antagonists."
+              description="A Python project that organizes, analyzes, and generates pdf reports for several U.S. drug markets, including ALK inhibitors, metastatic breast cancer drugs, norepinephrine and vasopressin, oral iron chelators, NK1 receptor antagonists and 5-HT3 receptor antagonists."
               shortDescription="A python project that organizes, analyzes, and generates pdf reports for several drug markets."
-              tech="Python 3, Sqlite, Peewee, Pandas, Numpy, Matplotlib, Xlsxwriter, Html5, Css3, Babel, Selenium, Unittest, Jupyter Notebook, Ipython"
+              tech="Python 3, SQLite, Peewee, Pandas, NumPy, Matplotlib, XlsxWriter, HTML5, CSS3, Babel, Selenium, Unittest, Jupyter Notebook, IPython"
               url=""
             />
           </Row>
